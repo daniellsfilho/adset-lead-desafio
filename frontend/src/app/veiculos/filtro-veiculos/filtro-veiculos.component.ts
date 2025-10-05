2a84e38a27d06a6d958cd5af4e99776f5c9d2cc4
Daniel Filho <daniel.2308@outlook.com>
2025-10-05 14:41:07 -0300

    Finalizando filtro

--- frontend/src/app/veiculos/filtro-veiculos/filtro-veiculos.component.ts
+++ frontend/src/app/veiculos/filtro-veiculos/filtro-veiculos.component.ts
@@ -1,8 +1,17 @@
 import { Component } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faEraser, faGlasses, faSearch, faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: 'filtro-veiculos',
     templateUrl: 'filtro-veiculos.component.html',
-    styleUrl: 'filtro-veiculos.component.scss'
+    styleUrl: 'filtro-veiculos.component.scss',
+    imports: [FontAwesomeModule]
 })
-export class FiltroVeiculosComponent { }
+export class FiltroVeiculosComponent {
+    faEraser = faEraser;
+    faGlass = faGlasses;
+    faSearch = faSearch;
+    faSortDown = faSortDown;
+    faSortUp = faSortUp;
+}
